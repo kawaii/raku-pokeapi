4566fbed3368f0070b18e8985478435af6afd498
Kane Valentine <kane@cute.im>
2019-04-19 00:40:15 +0100

    exeggutor example to showcase form differences

---
+++ example-pokemon/exeggutor.sql
@@ -0,0 +1,70 @@
+INSERT INTO pokeapi_pokedex VALUES (103, 'Exeggutor', 'Coconut Pokémon','[ "default", "alolan" ]',
+$$
+{
+    "default": [ "Grass", "Psychic" ],
+    "alolan": ["Grass", "Dragon"]
+}
+$$,
+$$
+{
+    "default": {
+        "Male": 0.50, "Female": 0.50
+   }
+}
+$$,
+$$
+{
+    "0": "Exeggcute", "1": "Exeggutor"
+}
+$$,
+$$
+{
+    "default": [ 2.0 ],
+    "alolan": [ 10.9 ]
+}
+$$,
+$$
+{
+    "default": [ 120.0 ],
+    "alolan": [ 415.6 ]
+}
+$$,
+45,
+$$
+{
+    "default": [ "Yellow" ]
+}
+$$,
+$$
+{
+    "default": {
+        "HP": 95,
+        "ATTACK": 95,
+        "DEFENCE": 85,
+        "SPECIAL ATTACK": 125,
+        "SPECIAL DEFENCE": 75,
+        "SPEED": 55
+    },
+    "alolan": {
+        "HP": 95,
+        "ATTACK": 105,
+        "DEFENCE": 85,
+        "SPECIAL ATTACK": 125,
+        "SPECIAL DEFENCE": 75,
+        "SPEED": 45
+    }
+}
+$$,
+$$
+{
+    "default": [ "Chlorophyll" ],
+    "alolan": [ "Frisk" ]
+}
+$$,
+$$
+{
+    "default": [ "Harvest" ]
+}
+$$,
+'[ "Grass" ]'
+);
