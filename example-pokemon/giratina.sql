b2f0267f52eb785085928170cb2df1fe35936132
Kane Valentine <kane@cute.im>
2019-04-18 21:41:52 +0100

    example database insert for giratina

---
+++ example-pokemon/giratina.sql
@@ -0,0 +1,59 @@
+INSERT INTO pokeapi_pokedex VALUES (487, 'Giratina', 'Renegade Pokémon','[ "altered", "origin" ]',
+$$
+{
+    "default": [ "Ghost", "Dragon" ]
+}
+$$,
+NULL,
+NULL,
+$$
+{
+    "altered": [ 4.5 ],
+    "origin": [ 6.9 ]
+}
+$$,
+$$
+{
+    "altered": [ 750.0 ],
+    "origin": [ 650.0 ]
+}
+$$,
+3,
+$$
+{
+    "default": [ "Black" ]
+}
+$$,
+$$
+{
+    "altered": {
+        "HP": 150,
+        "ATTACK": 100,
+        "DEFENCE": 120,
+        "SPECIAL ATTACK": 100,
+        "SPECIAL DEFENCE": 120,
+        "SPEED": 90
+    },
+    "origin": {
+        "HP": 150,
+        "ATTACK": 120,
+        "DEFENCE": 100,
+        "SPECIAL ATTACK": 120,
+        "SPECIAL DEFENCE": 100,
+        "SPEED": 90
+    }
+}
+$$,
+$$
+{
+    "altered": [ "Pressure" ],
+    "origin": [ "Levitate" ]
+}
+$$,
+$$
+{
+    "altered": [ "Telepathy" ]
+}
+$$,
+'[ "Undiscovered" ]'
+);
