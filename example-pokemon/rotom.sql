5847abc5f165cc2cb761054aa9b1d6d68d036a05
Kane Valentine <kane@cute.im>
2019-04-24 17:13:19 +0100

    add rotom example since it has a lot of forms

---
+++ example-pokemon/rotom.sql
@@ -0,0 +1,97 @@
+INSERT INTO pokeapi_pokedex VALUES (479, 'Rotom', 'Plasma Pokémon','[ "default", "fan", "frost", "heat", "mow", "wash" ]',
+$$
+{
+    "default": [ "Electric", "Ghost" ],
+    "fan": ["Electric", "Flying"],
+    "frost": ["Electric", "Ice"],
+    "heat": ["Electric", "Fire"],
+    "mow": ["Electric", "Grass"],
+    "wash": ["Electric", "Water"]
+}
+$$,
+NULL,
+$$
+{
+    "default": 0.3
+}
+$$,
+$$
+{
+    "default": 0.3
+}
+$$,
+NULL,
+45,
+$$
+{
+    "default": [ "Red" ]
+}
+$$,
+$$
+{
+    "default": {
+        "HP": 50,
+        "ATTACK": 50,
+        "DEFENCE": 77,
+        "SPECIAL ATTACK": 95,
+        "SPECIAL DEFENCE": 77,
+        "SPEED": 91
+    },
+    "fan": {
+        "HP": 50,
+        "ATTACK": 65,
+        "DEFENCE": 107,
+        "SPECIAL ATTACK": 105,
+        "SPECIAL DEFENCE": 107,
+        "SPEED": 86
+    },
+    "frost": {
+        "HP": 50,
+        "ATTACK": 65,
+        "DEFENCE": 107,
+        "SPECIAL ATTACK": 105,
+        "SPECIAL DEFENCE": 107,
+        "SPEED": 86
+    },
+    "heat": {
+        "HP": 50,
+        "ATTACK": 65,
+        "DEFENCE": 107,
+        "SPECIAL ATTACK": 105,
+        "SPECIAL DEFENCE": 107,
+        "SPEED": 86
+    },
+    "mow": {
+        "HP": 50,
+        "ATTACK": 65,
+        "DEFENCE": 107,
+        "SPECIAL ATTACK": 105,
+        "SPECIAL DEFENCE": 107,
+        "SPEED": 86
+    },
+    "wash": {
+        "HP": 50,
+        "ATTACK": 65,
+        "DEFENCE": 107,
+        "SPECIAL ATTACK": 105,
+        "SPECIAL DEFENCE": 107,
+        "SPEED": 86
+    }
+}
+$$,
+$$
+{
+    "default": [ "Levitate" ]
+}
+$$,
+NULL,
+'Medium Fast',
+70,
+$$
+{
+    "Sinnoh": 152, "Kalos Mountain": 68
+}
+$$,
+'[ "Amorphous" ]',
+5268
+);
