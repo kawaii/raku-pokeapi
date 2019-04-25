1278d778fdab2a09555ae5b58755e22a4084f917
Kane Valentine <kane@cute.im>
2019-04-25 14:10:12 +0100

    add natures into database

--- database-schema.sql
+++ database-schema.sql
@@ -34,6 +34,42 @@ CREATE TABLE IF NOT EXISTS pokeapi_evolutions (
     PRIMARY KEY (species, evolution, form)
 );
 
+CREATE TABLE IF NOT EXISTS pokeapi_natures (
+    nature text,
+    "increased-stat" text,
+    "decreased-stat" text,
+    "favorite-flavor" text,
+    "disliked-flavor" text,
+    PRIMARY KEY (nature)
+);
+
+INSERT INTO pokeapi_natures (nature, "increased-stat", "decreased-stat", "favorite-flavor", "disliked-flavor")
+VALUES ('Hardy', NULL, NULL, NULL, NULL),
+       ('Lonely', 'attack', 'defence', 'Spicy', 'Sour'),
+       ('Brave', 'attack', 'speed', 'Spicy', 'Sweet'),
+       ('Adamant', 'attack', 'special-attack', 'Spicy', 'Dry'),
+       ('Naughty', 'attack', 'special-defence', 'Spicy', 'Bitter'),
+       ('Bold', 'defence', 'attack', 'Sour', 'Spicy'),
+       ('Docile', NULL, NULL, NULL, NULL),
+       ('Relaxed', 'defence', 'speed', 'Sour', 'Sweet'),
+       ('Impish', 'defence', 'special-attack', 'Sour', 'Dry'),
+       ('Lax', 'defence', 'special-defence', 'Sour', 'Bitter'),
+       ('Timid', 'speed', 'attack', 'Sweet', 'Spicy'),
+       ('Hasty', 'speed', 'defence', 'Sweet', 'Sour'),
+       ('Serious', NULL, NULL, NULL, NULL),
+       ('Jolly', 'speed', 'special-attack', 'Sweet', 'Dry'),
+       ('Naive', 'speed', 'special-defence', 'Sweet', 'Bitter'),
+       ('Modest', 'special-attack', 'attack', 'Dry', 'Spicy'),
+       ('Mild', 'special-attack', 'defence', 'Dry', 'Sour'),
+       ('Quiet', 'special-attack', 'speed', 'Dry', 'Sweet'),
+       ('Bashful', NULL, NULL, NULL, NULL),
+       ('Rash', 'special-attack', 'special-defence', 'Dry', 'Bitter'),
+       ('Calm', 'special-defence', 'attack', 'Bitter', 'Spicy'),
+       ('Gentle', 'special-defence', 'defence', 'Bitter', 'Sour'),
+       ('Sassy', 'special-defence', 'speed', 'Bitter', 'Sweet'),
+       ('Careful', 'special-defence', 'special-attack', 'Bitter', 'Dry'),
+       ('Quirky', NULL, NULL, NULL, NULL);
+
 CREATE TABLE IF NOT EXISTS pokeapi_moves (
     id serial,
     name text,
