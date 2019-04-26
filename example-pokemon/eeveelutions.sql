cda5e61890088882d5819833e7a5f3054a0eff9a
Kane Valentine <kane@cute.im>
2019-04-26 17:31:02 +0100

    demo data for Eevee lineage to test evolution conditionals

---
+++ example-pokemon/eeveelutions.sql
@@ -0,0 +1,575 @@
+INSERT INTO pokeapi_pokedex VALUES (133, 'Eevee', 'Evolution Pokémon','[ "default" ]',
+$$
+{
+    "default": [ "Normal" ]
+}
+$$,
+$$
+{
+    "default": {
+        "male": 87.5, "female": 12.5
+   }
+}
+$$,
+$$
+{
+    "default": 0.3
+}
+$$,
+$$
+{
+    "default": 6.5
+}
+$$,
+NULL,
+45,
+$$
+{
+    "default": [ "Brown" ]
+}
+$$,
+$$
+{
+    "default": {
+        "hp": 55,
+        "attack": 55,
+        "defence": 50,
+        "special-attack": 45,
+        "special-defence": 65,
+        "speed": 55
+    }
+}
+$$,
+$$
+{
+    "default": [ "Run Away", "Adaptability" ]
+}
+$$,
+$$
+{
+    "default": [ "Anticipation" ]
+}
+$$,
+'Medium Fast',
+70,
+$$
+{
+    "Kanto": 133, "Johto": 184, "Sinnoh": 163, "Unova": 91, "Coastal Kalos": 77, "Alola": 123
+}
+$$,
+'[ "Field" ]',
+9123
+);
+
+INSERT INTO pokeapi_pokedex VALUES (134, 'Vaporeon', 'Bubble Jet Pokémon','[ "default" ]',
+$$
+{
+    "default": [ "Water" ]
+}
+$$,
+$$
+{
+    "default": {
+        "male": 87.5, "female": 12.5
+   }
+}
+$$,
+$$
+{
+    "default": 1.0
+}
+$$,
+$$
+{
+    "default": 29.0
+}
+$$,
+NULL,
+45,
+$$
+{
+    "default": [ "Blue" ]
+}
+$$,
+$$
+{
+    "default": {
+        "hp": 130,
+        "attack": 65,
+        "defence": 60,
+        "special-attack": 110,
+        "special-defence": 95,
+        "speed": 65
+    }
+}
+$$,
+$$
+{
+    "default": [ "Water Absorb" ]
+}
+$$,
+$$
+{
+    "default": [ "Hydration" ]
+}
+$$,
+'Medium Fast',
+70,
+$$
+{
+    "Kanto": 134, "Johto": 185, "Sinnoh": 164, "Unova": 92, "Coastal Kalos": 78, "Alola": 124
+}
+$$,
+'[ "Field" ]',
+9123
+);
+
+INSERT INTO pokeapi_pokedex VALUES (135, 'Jolteon', 'Lightning Pokémon','[ "default" ]',
+$$
+{
+    "default": [ "Electric" ]
+}
+$$,
+$$
+{
+    "default": {
+        "male": 87.5, "female": 12.5
+   }
+}
+$$,
+$$
+{
+    "default": 0.8
+}
+$$,
+$$
+{
+    "default": 24.5
+}
+$$,
+NULL,
+45,
+$$
+{
+    "default": [ "Yellow" ]
+}
+$$,
+$$
+{
+    "default": {
+        "hp": 65,
+        "attack": 65,
+        "defence": 60,
+        "special-attack": 110,
+        "special-defence": 95,
+        "speed": 130
+    }
+}
+$$,
+$$
+{
+    "default": [ "Volt Absorb" ]
+}
+$$,
+$$
+{
+    "default": [ "Quick Feet" ]
+}
+$$,
+'Medium Fast',
+70,
+$$
+{
+    "Kanto": 135, "Johto": 186, "Sinnoh": 165, "Unova": 93, "Coastal Kalos": 79, "Alola": 125
+}
+$$,
+'[ "Field" ]',
+9123
+);
+
+INSERT INTO pokeapi_pokedex VALUES (136, 'Flareon', 'Flame Pokémon','[ "default" ]',
+$$
+{
+    "default": [ "Fire" ]
+}
+$$,
+$$
+{
+    "default": {
+        "male": 87.5, "female": 12.5
+   }
+}
+$$,
+$$
+{
+    "default": 0.9
+}
+$$,
+$$
+{
+    "default": 25.0
+}
+$$,
+NULL,
+45,
+$$
+{
+    "default": [ "Red" ]
+}
+$$,
+$$
+{
+    "default": {
+        "hp": 65,
+        "attack": 130,
+        "defence": 60,
+        "special-attack": 95,
+        "special-defence": 110,
+        "speed": 65
+    }
+}
+$$,
+$$
+{
+    "default": [ "Flash Fire" ]
+}
+$$,
+$$
+{
+    "default": [ "Guts" ]
+}
+$$,
+'Medium Fast',
+70,
+$$
+{
+    "Kanto": 136, "Johto": 187, "Sinnoh": 166, "Unova": 94, "Coastal Kalos": 80, "Alola": 126
+}
+$$,
+'[ "Field" ]',
+9123
+);
+
+INSERT INTO pokeapi_pokedex VALUES (196, 'Espeon', 'Sun Pokémon','[ "default" ]',
+$$
+{
+    "default": [ "Psychic" ]
+}
+$$,
+$$
+{
+    "default": {
+        "male": 87.5, "female": 12.5
+   }
+}
+$$,
+$$
+{
+    "default": 0.9
+}
+$$,
+$$
+{
+    "default": 26.5
+}
+$$,
+NULL,
+45,
+$$
+{
+    "default": [ "Purple" ]
+}
+$$,
+$$
+{
+    "default": {
+        "hp": 65,
+        "attack": 65,
+        "defence": 60,
+        "special-attack": 130,
+        "special-defence": 95,
+        "speed": 110
+    }
+}
+$$,
+$$
+{
+    "default": [ "Synchronize" ]
+}
+$$,
+$$
+{
+    "default": [ "Magic Bounce" ]
+}
+$$,
+'Medium Fast',
+70,
+$$
+{
+    "Johto": 188, "Sinnoh": 167, "Unova": 95, "Coastal Kalos": 81, "Alola": 127
+}
+$$,
+'[ "Field" ]',
+9123
+);
+
+INSERT INTO pokeapi_pokedex VALUES (197, 'Umbreon', 'Moonlight Pokémon','[ "default" ]',
+$$
+{
+    "default": [ "Dark" ]
+}
+$$,
+$$
+{
+    "default": {
+        "male": 87.5, "female": 12.5
+   }
+}
+$$,
+$$
+{
+    "default": 1.0
+}
+$$,
+$$
+{
+    "default": 27.0
+}
+$$,
+NULL,
+45,
+$$
+{
+    "default": [ "Black" ]
+}
+$$,
+$$
+{
+    "default": {
+        "hp": 95,
+        "attack": 65,
+        "defence": 110,
+        "special-attack": 60,
+        "special-defence": 130,
+        "speed": 65
+    }
+}
+$$,
+$$
+{
+    "default": [ "Synchronize" ]
+}
+$$,
+$$
+{
+    "default": [ "Inner Focus" ]
+}
+$$,
+'Medium Fast',
+70,
+$$
+{
+    "Johto": 189, "Sinnoh": 168, "Unova": 96, "Coastal Kalos": 82, "Alola": 128
+}
+$$,
+'[ "Field" ]',
+9123
+);
+
+INSERT INTO pokeapi_pokedex VALUES (470, 'Leafeon', 'Verdant Pokémon','[ "default" ]',
+$$
+{
+    "default": [ "Grass" ]
+}
+$$,
+$$
+{
+    "default": {
+        "male": 87.5, "female": 12.5
+   }
+}
+$$,
+$$
+{
+    "default": 1.0
+}
+$$,
+$$
+{
+    "default": 25.5
+}
+$$,
+NULL,
+45,
+$$
+{
+    "default": [ "Green" ]
+}
+$$,
+$$
+{
+    "default": {
+        "hp": 65,
+        "attack": 110,
+        "defence": 130,
+        "special-attack": 60,
+        "special-defence": 65,
+        "speed": 95
+    }
+}
+$$,
+$$
+{
+    "default": [ "Leaf Guard" ]
+}
+$$,
+$$
+{
+    "default": [ "Chlorophyll" ]
+}
+$$,
+'Medium Fast',
+70,
+$$
+{
+    "Sinnoh": 169, "Unova": 97, "Coastal Kalos": 83, "Alola": 129
+}
+$$,
+'[ "Field" ]',
+9123
+);
+
+INSERT INTO pokeapi_pokedex VALUES (471, 'Glaceon', 'Fresh Snow Pokémon','[ "default" ]',
+$$
+{
+    "default": [ "Ice" ]
+}
+$$,
+$$
+{
+    "default": {
+        "male": 87.5, "female": 12.5
+   }
+}
+$$,
+$$
+{
+    "default": 0.8
+}
+$$,
+$$
+{
+    "default": 25.9
+}
+$$,
+NULL,
+45,
+$$
+{
+    "default": [ "Blue" ]
+}
+$$,
+$$
+{
+    "default": {
+        "hp": 65,
+        "attack": 60,
+        "defence": 110,
+        "special-attack": 130,
+        "special-defence": 95,
+        "speed": 65
+    }
+}
+$$,
+$$
+{
+    "default": [ "Snow Cloak" ]
+}
+$$,
+$$
+{
+    "default": [ "Ice Body" ]
+}
+$$,
+'Medium Fast',
+70,
+$$
+{
+    "Sinnoh": 170, "Unova": 98, "Coastal Kalos": 84, "Alola": 130
+}
+$$,
+'[ "Field" ]',
+9123
+);
+
+INSERT INTO pokeapi_pokedex VALUES (700, 'Sylveon', 'Intertwining Pokémon','[ "default" ]',
+$$
+{
+    "default": [ "Fairy" ]
+}
+$$,
+$$
+{
+    "default": {
+        "male": 87.5, "female": 12.5
+   }
+}
+$$,
+$$
+{
+    "default": 1.0
+}
+$$,
+$$
+{
+    "default": 23.5
+}
+$$,
+NULL,
+45,
+$$
+{
+    "default": [ "Pink" ]
+}
+$$,
+$$
+{
+    "default": {
+        "hp": 95,
+        "attack": 65,
+        "defence": 65,
+        "special-attack": 110,
+        "special-defence": 130,
+        "speed": 60
+    }
+}
+$$,
+$$
+{
+    "default": [ "Cute Charm" ]
+}
+$$,
+$$
+{
+    "default": [ "Pixilate" ]
+}
+$$,
+'Medium Fast',
+70,
+$$
+{
+    "Coastal Kalos": 85, "Alola": 131
+}
+$$,
+'[ "Field" ]',
+9123
+);
+
+INSERT INTO pokeapi_evolutions VALUES ('Eevee', 'Vaporeon', 'default', '{"stone": "Water Stone"}');
+INSERT INTO pokeapi_evolutions VALUES ('Eevee', 'Jolteon', 'default', '{"stone": "Thunder Stone"}');
+INSERT INTO pokeapi_evolutions VALUES ('Eevee', 'Flareon', 'default', '{"stone": "Fire Stone"}');
+INSERT INTO pokeapi_evolutions VALUES ('Eevee', 'Espeon', 'default', '{"friendship": 220, "time": "day", "event": "level-up"}');
+INSERT INTO pokeapi_evolutions VALUES ('Eevee', 'Umbreon', 'default', '{"friendship": 220, "time": "night", "event": "level-up"}');
+INSERT INTO pokeapi_evolutions VALUES ('Eevee', 'Leafeon', 'default', '{"proximity": "Moss Rock", "event": "level-up"}');
+INSERT INTO pokeapi_evolutions VALUES ('Eevee', 'Glaceon', 'default', '{"proximity": "Ice Rock", "event": "level-up"}');
+INSERT INTO pokeapi_evolutions VALUES ('Eevee', 'Sylveon', 'default', '{"affection": 2, "moveset": [ { "type": "Fairy" } ], "event": "level-up"}');
